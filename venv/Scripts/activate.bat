e27a26dafd95c7f6b1d712928aafbd523f00f93a
WathsalaM369 <wathsala.ghrm@gmail.com>
2025-10-16 14:58:18 +0530

    last2

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Vinusha Perera\OneDrive\Desktop\IT Project\stress_management_coach\venv"
+set "VIRTUAL_ENV=C:\Users\waths\stress_management_coach\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
